77e9391f81fe2f6b7b5ce7cb939291be9d9cfd98
Maurice Koster <maurice.koster@innolan.nl>
2016-10-21 16:37:10 +0200

    Stuf

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css', './app.scss']
+})
+export class AppComponent {
+  title = 'Herbert works!';
+}
